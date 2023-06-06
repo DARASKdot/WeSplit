f764f067fa50fa67bcf6bc38bd45f60e78790634
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-06-06 10:44:15 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hiroshi Inoue on R 5/06/06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
